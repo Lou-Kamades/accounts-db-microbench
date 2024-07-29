16995535ba218256fc8f7695e6e59bfc6f7dbd5e
Lou-Kamades <lou_kamades@proton.me>
2024-07-29 11:13:47 -0500

    add agave snapshot loading

--- src/main.rs
+++ src/main.rs
@@ -1,305 +1,196 @@
-use solana_accounts_db::account_storage::meta::StorableAccountsWithHashes;
-/// cargo run --release
-
+use solana_accounts_db::account_storage::AccountStorageMap;
+use solana_accounts_db::accounts_db::AtomicAccountsFileId;
+use solana_accounts_db::accounts_file::StorageAccess;
+use solana_accounts_db::accounts_index::AccountSecondaryIndexes;
+use solana_measure::measure;
+use solana_runtime::serde_snapshot::fields_from_streams;
+use solana_runtime::serde_snapshot::reconstruct_accountsdb_from_fields;
+use solana_runtime::snapshot_utils::deserialize_snapshot_data_files;
+use solana_runtime::snapshot_utils::get_highest_bank_snapshot_post;
+use solana_runtime::snapshot_utils::snapshot_storage_rebuilder::SnapshotStorageRebuilder;
+use solana_runtime::snapshot_utils::streaming_snapshot_dir_files;
+use solana_accounts_db::accounts_db::AccountShrinkThreshold;
 use solana_accounts_db::accounts_db::AccountsDb;
 use solana_accounts_db::accounts_db::ACCOUNTS_DB_CONFIG_FOR_BENCHMARKS;
-use solana_accounts_db::ancestors::Ancestors;
-use solana_sdk::rent_collector::RentCollector;
-use solana_sdk::account::AccountSharedData;
-use solana_sdk::genesis_config::ClusterType;
-use solana_accounts_db::accounts_db::AccountShrinkThreshold;
+use solana_runtime::snapshot_utils::snapshot_storage_rebuilder::RebuiltSnapshotStorage;
+use solana_runtime::snapshot_utils::BankSnapshotInfo;
+use solana_runtime::snapshot_utils::SnapshotFrom;
+use solana_runtime::snapshot_utils::SnapshotRootPaths;
+use solana_runtime::snapshot_utils::StorageAndNextAccountsFileId;
+use solana_runtime::snapshot_utils::SNAPSHOT_VERSION_FILENAME;
 
+use std::env;
 use std::fmt::Result;
+use std::fs;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
+use std::sync::atomic::AtomicBool;
 use std::sync::Arc;
-use solana_sdk::account::ReadableAccount;
-use solana_accounts_db::u64_align;
-use solana_accounts_db::accounts_file::ALIGN_BOUNDARY_OFFSET;
 
-use solana_accounts_db::accounts_hash::AccountHash;
-use solana_sdk::hash::Hash;
-
-use solana_accounts_db::accounts_db::StorageSizeAndCountMap;
-use solana_accounts_db::accounts_index_storage::Startup;
-use solana_accounts_db::accounts_index::IndexLimitMb;
-
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum MemoryLocation {
-    Ram,
-    Disk
+pub fn main() -> Result {
+    let _db = crate::load_snapshot().unwrap();
+    return Ok(());
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct Benchmark { 
-    n_accounts: usize,
-    slot_list_len: usize,
-    accounts: MemoryLocation,
-    index: MemoryLocation,
-    n_accounts_multiple: usize,
-}
+pub fn load_snapshot() -> anyhow::Result<AccountsDb> {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
 
-pub fn run_benchmark(benchmark: Benchmark) -> Result {
-    println!("benchmark {:?}", benchmark);
-    let mut config = ACCOUNTS_DB_CONFIG_FOR_BENCHMARKS;
-    if benchmark.index == MemoryLocation::Ram {
-        // use RAM indexes 
-        config.index.as_mut().unwrap().index_limit_mb = IndexLimitMb::InMemOnly;
-    } else { 
-        // use DISK indexes 
-        config.index.as_mut().unwrap().index_limit_mb = IndexLimitMb::Unspecified;
+    let data_dir = &args[1];
+    let slot: u64 = args[2].parse().unwrap();
+
+    let account_paths_str = &format!("{}/accounts", data_dir);
+    let account_paths = vec![account_paths_str.into()];
+    let snapshot_path = format!("{}/snapshots/{}", data_dir, slot);
+
+    let version_path = format!("{}/version", snapshot_path);
+    let completion_flag_path = format!("{}/state_complete", snapshot_path);
+    let status_cache_path = format!("{}/status_cache", snapshot_path);
+
+    if !fs::metadata(&completion_flag_path).is_ok() {
+        fs::File::create(completion_flag_path).unwrap();
+    }
+    if !fs::metadata(&version_path).is_ok() {
+        let mut version_file = File::create(version_path).expect("create version failed");
+        version_file
+            .write("1.2.0".as_bytes())
+            .expect("write version failed");
+    }
+    if !fs::metadata(&status_cache_path).is_ok() {
+        fs::File::create(status_cache_path).unwrap();
     }
 
-    let n_accounts = benchmark.n_accounts;
-    let slot_list_len = benchmark.slot_list_len;
-    let accounts_db = AccountsDb::new_with_config(
-        vec![], 
-        &ClusterType::Development, 
-        solana_accounts_db::accounts_index::AccountSecondaryIndexes::default(),
-        AccountShrinkThreshold::default(),
-        Some(config),
-        None,
-        Arc::default(),
+    let exit = Arc::new(AtomicBool::new(false));
+
+    let mut accounts_db_config = ACCOUNTS_DB_CONFIG_FOR_BENCHMARKS;
+    accounts_db_config.base_working_path = Some(data_dir.clone().into());
+    accounts_db_config.accounts_hash_cache_path = Some(
+        format!(
+            "{}/{}",
+            data_dir,
+            AccountsDb::DEFAULT_ACCOUNTS_HASH_CACHE_DIR
+        )
+        .into(),
     );
-    let mut pubkeys = vec![];
-    for _ in 0..n_accounts { 
-        pubkeys.push(solana_sdk::pubkey::new_rand());
-    }
 
-    let mut slots_used = 0;
-    if benchmark.accounts == MemoryLocation::Disk {
-        let static_account_size = 136;
-        let mut total_size = 0;
-        for i in 0..n_accounts {
-            total_size += static_account_size + (i % 1_000); 
-            total_size = u64_align!(total_size);
-        }
-        let total_size_u64 = total_size as u64;
-        
-        for s in 0..(slot_list_len + benchmark.n_accounts_multiple) { 
-            let storage = accounts_db.create_and_insert_store(s as u64, total_size_u64, "blah");
-            for i in 0..n_accounts {
-                let account = AccountSharedData::new(
-                    10, 
-                    i % 1_000, 
-                    AccountSharedData::default().owner()
-                );
+    // mostly taken from bank_from_snapshot_dir
+    println!("{:?}", snapshot_path);
+    let bank_snapshot_info = get_highest_bank_snapshot_post(format!("{}/snapshots", data_dir))
+        .expect("Could not find snapshot");
+    let next_append_vec_id = Arc::new(AtomicAccountsFileId::new(0));
+    let storage_access = accounts_db_config.storage_access.clone();
 
-                let hashes = vec![AccountHash(Hash::default()); 1];
-                let account_inner = &[(&pubkeys[i % n_accounts], &account)][..];
-                let accounts = (s as u64, account_inner);
-                let storable_accounts = &StorableAccountsWithHashes::new_with_hashes(
-                    &accounts,
-                    hashes,
-                );
-                storage.accounts.append_accounts(
-                    storable_accounts,
-                    0,
-                );
-            }
-        }
+    let (storage, measure_rebuild_storages) = measure!(
+        rebuild_storages_from_snapshot_dir(
+            &bank_snapshot_info,
+            &account_paths,
+            next_append_vec_id.clone(),
+            storage_access,
+        )?,
+        "rebuild storages from snapshot dir"
+    );
+    println!("{}", measure_rebuild_storages);
+    // println!("{:?}", storage);
 
-        let bins = accounts_db.accounts_index.bins();
-        accounts_db.accounts_index.set_startup(Startup::Startup);
+    let next_append_vec_id =
+        Arc::try_unwrap(next_append_vec_id).expect("this is the only strong reference");
+    let storage_and_next_append_vec_id = StorageAndNextAccountsFileId {
+        storage,
+        next_append_vec_id,
+    };
 
-        let mut store_id = 0;
-        accounts_db.add_root(0);
+    let snapshot_root_paths = SnapshotRootPaths {
+        full_snapshot_root_file_path: bank_snapshot_info.snapshot_path(),
+        incremental_snapshot_root_file_path: None,
+    };
 
-        for s in 0..benchmark.n_accounts_multiple {
-            let storage = accounts_db.storage.get_slot_storage_entry(s as u64).unwrap();
-            let storage_info = StorageSizeAndCountMap::default();
-            accounts_db.generate_index_for_slot(
-                &storage,
-                s as u64,
-                store_id,
-                &RentCollector::default(),
-                &storage_info,
+    let ((accounts_db, _reconstructed_accounts_db_info), account_deser_measure) = measure!(
+        deserialize_snapshot_data_files(&snapshot_root_paths, |snapshot_streams| {
+            let (snapshot_bank_fields, snapshot_accounts_db_fields) =
+                fields_from_streams(snapshot_streams)?;
+
+            let capitalizations = (
+                snapshot_bank_fields.full.capitalization,
+                snapshot_bank_fields
+                    .incremental
+                    .as_ref()
+                    .map(|bank_fields| bank_fields.capitalization),
             );
-            store_id += 1;
-        }
-        (0..bins).for_each(|pubkey_bin| {
-            let r_account_maps = &accounts_db.accounts_index.account_maps[pubkey_bin];
-            r_account_maps.write_startup_info_to_disk();
-        });
+            let bank_fields = snapshot_bank_fields.collapse_into();
 
-        let timer = std::time::Instant::now();
+            let (accounts_db, reconstructed_accounts_db_info) = reconstruct_accountsdb_from_fields(
+                snapshot_accounts_db_fields,
+                &account_paths,
+                storage_and_next_append_vec_id,
+                &solana_sdk::genesis_config::GenesisConfig::default(),
+                AccountSecondaryIndexes::default(),
+                None, //limit_load_slot_count_from_snapshot,
+                AccountShrinkThreshold::default(),
+                false, // verify_index
+                Some(accounts_db_config),
+                None, // accounts_update_notifier
+                exit,
+                bank_fields.epoch_accounts_hash,
+                capitalizations,
+                bank_fields.incremental_snapshot_persistence.as_ref(),
+            )?;
 
-        for s in benchmark.n_accounts_multiple..(benchmark.n_accounts_multiple + slot_list_len) {
-            let storage = accounts_db.storage.get_slot_storage_entry(s as u64).unwrap();
-            let storage_info = StorageSizeAndCountMap::default();
-            accounts_db.generate_index_for_slot(
-                &storage,
-                s as u64,
-                store_id,
-                &RentCollector::default(),
-                &storage_info,
-            );
-            store_id += 1;
-        }
-        (0..bins).for_each(|pubkey_bin| {
-            let r_account_maps = &accounts_db.accounts_index.account_maps[pubkey_bin];
-            r_account_maps.write_startup_info_to_disk();
-        });
+            Ok((accounts_db, reconstructed_accounts_db_info))
+        })?,
+        "reconstruct_accountsdb_from_fields"
+    );
 
-        slots_used = slot_list_len + benchmark.n_accounts_multiple;
-        let elapsed = timer.elapsed();
-        println!("WRITE elapsed: {}", elapsed.as_nanos());
-    } else { 
-        let mut accounts = vec![];
-        let mut refs = vec![];
+    println!("{}", account_deser_measure);
 
-        for _ in 0..(slot_list_len + benchmark.n_accounts_multiple) {
-            for i in 0..n_accounts { 
-                let account = AccountSharedData::new(
-                    10, 
-                    i % 1000, 
-                    AccountSharedData::default().owner()
-                );
-                accounts.push(account);
-            }
-        }
-
-        // initial account amounts 
-        accounts_db.add_root(0);
-
-        let mut slot = 0;
-        let mut no_bench_refs = vec![];
-        for i in 0..(n_accounts * benchmark.n_accounts_multiple) {
-            no_bench_refs.push((&pubkeys[i % n_accounts], &accounts[i]));
-        }
-        if no_bench_refs.len() > 0 { 
-            accounts_db.store_for_tests(0 as u64, &no_bench_refs);
-            slot += 1;
-        } 
-
-        // the rest
-        for i in (n_accounts * benchmark.n_accounts_multiple)..(n_accounts * (slot_list_len + benchmark.n_accounts_multiple)) {
-            refs.push((&pubkeys[i % n_accounts], &accounts[i]));
-        }
-        let timer = std::time::Instant::now();
-        let mut index = 0;
-        for s in slot..(slot_list_len + slot) {
-            let start_index = index;
-            let end_index = index + n_accounts;
-
-            accounts_db.store_for_tests(s as u64, &refs[start_index..end_index]);
-            index = end_index;
-        }
-        slot += slot_list_len;
-        slots_used = slot;
-        let elapsed = timer.elapsed();
-        println!("WRITE elapsed: {}", elapsed.as_nanos());
-    }
-
-    let mut slots = vec![];
-    for slot in 0..slots_used {
-        slots.push(slot as u64); 
-    }
-    let ancestors = Ancestors::from(slots);
-    let timer = std::time::Instant::now();
-    for i in 0..n_accounts {
-        let (account, _) = accounts_db.load_without_fixed_root(&ancestors, &pubkeys[i]).unwrap();
-        assert_eq!(account.data().len(), i % 1_000);
-    }
-
-    let elapsed = timer.elapsed();
-    println!("READ elapsed: {}", elapsed.as_nanos());
-
-    return Ok(());
+    Ok(accounts_db)
 }
 
-pub fn main() -> Result { 
-    let benches = vec![
-        // Benchmark { 
-        //     n_accounts: 100_000,
-        //     slot_list_len: 1,
-        //     accounts: MemoryLocation::Ram,
-        //     index: MemoryLocation::Ram,
-        //     n_accounts_multiple: 0,
-        // },
-        // Benchmark { 
-        //     n_accounts: 10_000,
-        //     slot_list_len: 10,
-        //     accounts: MemoryLocation::Ram,
-        //     index: MemoryLocation::Ram,
-        //     n_accounts_multiple: 0,
-        // },
+/// Performs the common tasks when deserializing a snapshot
+/// Removes hardlinks operations from the original function
+pub fn rebuild_storages_from_snapshot_dir(
+    snapshot_info: &BankSnapshotInfo,
+    account_paths: &[PathBuf],
+    next_append_vec_id: Arc<AtomicAccountsFileId>,
+    storage_access: StorageAccess,
+) -> anyhow::Result<AccountStorageMap> {
+    let bank_snapshot_dir = &snapshot_info.snapshot_dir;
 
-        // Benchmark { 
-        //     n_accounts: 10_000,
-        //     slot_list_len: 10,
-        //     accounts: MemoryLocation::Disk,
-        //     index: MemoryLocation::Ram,
-        //     n_accounts_multiple: 0,
-        // },
-        // Benchmark { 
-        //     n_accounts: 500_000,
-        //     slot_list_len: 1,
-        //     accounts: MemoryLocation::Disk,
-        //     index: MemoryLocation::Ram,
-        //     n_accounts_multiple: 0,
-        // },
-        // Benchmark { 
-        //     n_accounts: 500_000,
-        //     slot_list_len: 3,
-        //     accounts: MemoryLocation::Disk,
-        //     index: MemoryLocation::Ram,
-        //     n_accounts_multiple: 0,
-        // },
-        Benchmark { 
-            n_accounts: 3_000_000,
-            slot_list_len: 1,
-            accounts: MemoryLocation::Disk,
-            index: MemoryLocation::Ram,
-            n_accounts_multiple: 0,
-        },
-        // Benchmark { 
-        //     n_accounts: 3_000_000,
-        //     slot_list_len: 3,
-        //     accounts: MemoryLocation::Disk,
-        //     index: MemoryLocation::Ram,
-        //     n_accounts_multiple: 0,
-        // },
-        // Benchmark { 
-        //     n_accounts: 500_000,
-        //     slot_list_len: 1,
-        //     accounts: MemoryLocation::Disk,
-        //     index: MemoryLocation::Ram,
-        //     n_accounts_multiple: 2,
-        // },
+    let (file_sender, file_receiver) = crossbeam_channel::unbounded();
+    let snapshot_file_path = &snapshot_info.snapshot_path();
+    let snapshot_version_path = bank_snapshot_dir.join(SNAPSHOT_VERSION_FILENAME);
+    streaming_snapshot_dir_files(
+        file_sender,
+        snapshot_file_path,
+        snapshot_version_path,
+        account_paths,
+    )?;
 
-        // Benchmark { 
-        //     n_accounts: 500_000,
-        //     slot_list_len: 1,
-        //     accounts: MemoryLocation::Disk,
-        //     index: MemoryLocation::Disk,
-        //     n_accounts_multiple: 0,
-        // },
-        // Benchmark { 
-        //     n_accounts: 3_000_000,
-        //     slot_list_len: 1,
-        //     accounts: MemoryLocation::Disk,
-        //     index: MemoryLocation::Disk,
-        //     n_accounts_multiple: 0,
-        // },
-        // Benchmark { 
-        //     n_accounts: 500_000,
-        //     slot_list_len: 1,
-        //     accounts: MemoryLocation::Disk,
-        //     index: MemoryLocation::Disk,
-        //     n_accounts_multiple: 2,
-        // },
-    ];
+    let num_rebuilder_threads = num_cpus::get_physical().saturating_sub(1).max(1);
 
-    for benchmark in benches {
-        run_benchmark(benchmark)?;
-        println!("---");
-    }
+    let version_and_storages = SnapshotStorageRebuilder::rebuild_storage(
+        file_receiver,
+        num_rebuilder_threads,
+        next_append_vec_id,
+        SnapshotFrom::Dir,
+        storage_access,
+    )?;
 
-    // println!("done!");
-
-    return Ok(());
+    let RebuiltSnapshotStorage {
+        snapshot_version: _,
+        storage,
+    } = version_and_storages;
+    Ok(storage)
 }
 
-// pub fn main() {
-    
-// }
+#[cfg(test)]
+mod test {
+    use crate::gbs;
+    use solana_runtime::snapshot_utils::get_bank_snapshots;
+
+    #[test]
+    pub fn test_load_snapshot() -> anyhow::Result<()> {
+        let _db = crate::load_snapshot()?;
+        Ok(())
+    }
+}
